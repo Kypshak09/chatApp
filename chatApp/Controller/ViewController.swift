6473cc79ed7fb28f2bb20ac9a650ab2fda1a9974
Kypshak09 <amirzhunusov09@gmail.com>
2022-11-30 22:02:55 +0600

    animation with loop of the label

--- chatApp/Controller/ViewController.swift
+++ chatApp/Controller/ViewController.swift
@@ -16,6 +16,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        textLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "🗣 Chatssenger"
+        for i in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.textLabel.text?.append(i)
+            }
+            charIndex += 1
+        }
         
     }
 
